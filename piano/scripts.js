e139f48f9e9314f75fb85fe7182a7eb5d1ef46a7
Christie Hong <christie.hong97@gmail.com>
2018-12-08 11:19:16 -0500

    vectors are built, and ui notes are showing for user)

--- piano/scripts.js
+++ piano/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
 	var WHITE_KEYS = [1,3,5,6,8,10,12,13,15,17,18,20,22,24,25];
-	var SOUND_DICT = {1:'c',2:'cis',3:'d',4:'es',5:'e',6:'f',7:'fis',8:'g',9:'gis',10:'a',11:'b',12:'h',13:'c1',
-		14:'cis1',15:'d1',16:'es1',17:'e1',18:'f1',19:'fis1',20:'g1',21:'gis1',22:'a1',23:'b1',24:'h1',25:'c2'};
+
+	var SOUND_DICT = {1:'C',2:'C#',3:'D',4:'E♭',5:'E',6:'F',7:'F#',8:'G',9:'A♭',10:'A',11:'B♭',12:'B',13:'C',
+		14:'C#',15:'D',16:'E♭',17:'E',18:'F',19:'F#'};
+
 	var STYLE_DICT = {'to1':14,'l2':9,'r2':5,'to3':14,'l4':5,'r4':9,'to5':14,'to6':13,'l7':11,'r7':3,'to8':13,'l9':7,
 		'r9':7,'to10':13,'l11':3,'r11':11,'to12':13,'to13':14,'l14':9,'r14':5,'to15':14,'l16':5,'r16':9,'to17':14,
 		'to18':13,'l19':11,'r19':3,'to20':13,'l21':7,'r21':7,'to22':13,'l23':3,'r23':11,'to24':13,'to25':23,};
+
 	var MIDI_DICT = {65:1, 87:2, 83:3, 69:4, 68:5, 70:6, 84:7, 71:8, 89:9, 72:10, 85:11, 74:12, 75:13, 79:14, 76:15, 
 		80:16, 186:17, 222:18, 219:19}
 
@@ -64,15 +67,14 @@ $(document).ready(function() {
 	 	}
 	}
 
+
 	var trials = []
+	var interface_trials = []
 	function press(x) {
 		$('#song').text(function(index, text) {
-			trials.push("[" + Object.keys(heldKeys)+"]")
-			return trials.toString()
-		});
-
-		$('#frequencies').text(function(index, text) {
-			return text+FREQ_DICT[document.getElementById('freq').innerHTML]+",";
+			interface_trials.push("[" + Object.keys(heldKeys)+"]")
+			trials.push(Object.keys(heldKeysMidi))
+			return interface_trials.toString()
 		});
 	 	
 	 	removeSelection();
@@ -132,47 +134,62 @@ $(document).ready(function() {
 	loadPianoMusic();
 
 
-	var heldKeys = {};
-
+	var heldKeys = {}; // SOUND_DICT for UI purposes
+	var heldKeysMidi = {}; // MIDI_DICT for vector purposes 
 	$('#idbody').keydown(function(e) {
-		console.log("down")
-		if (e.keyCode in heldKeys) {
+		// console.log("down")
+		key = SOUND_DICT[MIDI_DICT[e.keyCode]]
+		if (key in heldKeys) {
 			return false;
 		}
-		heldKeys[e.keyCode]=true;
+		heldKeys[key]=true;
+		heldKeysMidi[MIDI_DICT[e.keyCode]] = true;
 		keywaspressed(e);
+
 	});
 
-	$("#idbody").keyup(function() {
-		console.log("up")
-		lastState=null;
-		delete heldKeys[event.keyCode];
-		id = MIDI_DICT[event.keyCode];
+	$("#idbody").keyup(function(e) {
+		// console.log("up")
+		key = SOUND_DICT[MIDI_DICT[e.keyCode]];
+		delete heldKeys[key];
+		delete heldKeysMidi[MIDI_DICT[e.keyCode]];
+		id = MIDI_DICT[e.keyCode];
 		unselectkey(id);
 	});
+
 
 	keyboardsize(0);
 
 	$('#clear').click(function() {
-		$('#frequencies').text('');
 		$('#song').text('');
 	});
 
-
+	
 	$('#save').click(function() {
-		console.log("trying save");
-		var xhttp;
-		var author = "Christie Hong";
-		var songName = "Simple";
-		xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-		  if (this.readyState == 4 && this.status == 200) {
-		    console.log(this.responseText);
-		  }
-		};
-		xhttp.open("GET", "https://iesc-s2.mit.edu/6S08dev/abertics/final/sb1.py?ADD=1&name="+songName+
-				"&author="+author+"&notes="+$('#song').text()+"&frequencies="+$('#frequencies').text(), true);
-		xhttp.send();
+		var vectors = [] // holds 20len vectors for each trial
+		for (var i=0; i<trials.length; i++) {
+			vector = []
+			for (var x=0; x<20; x++){
+				vector.push(0)
+			}
+
+			for (var j=0; j<trials[i].length; j++) {
+				vector[trials[i][j]-1] = 1
+			}
+			vectors.push(vector)
+		}
+		// var xhttp;
+		// var author = "Christie Hong";
+		// var songName = "Simple";
+		// xhttp = new XMLHttpRequest();
+		// xhttp.onreadystatechange = function() {
+		//   if (this.readyState == 4 && this.status == 200) {
+		//     console.log(this.responseText);
+		//   }
+		// };
+		// xhttp.open("GET", "https://iesc-s2.mit.edu/6S08dev/abertics/final/sb1.py?ADD=1&name="+songName+
+		// 		"&author="+author+"&notes="+$('#song').text()+"&frequencies="+$('#frequencies').text(), true);
+		// xhttp.send();
 
 	});
 
