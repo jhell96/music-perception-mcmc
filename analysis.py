43e49d997be872c4f0f0d2ad6d92dde20fd1ea62
Josh H <josh@unify.id>
2018-12-16 16:43:10 -0500

    updated analysis file to pull results

--- analysis.py
+++ analysis.py
@@ -1,3 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import piano.data.parse_results as pr
+
+PATH_TO_TRIALS = 'piano/data/raw/trials.txt'
+
+if __name__ == '__main__':
+    # example of fetching results from the web, and storing in trials.txt
+    results = pr.parse_results(file=PATH_TO_TRIALS, web_refresh=True)
+    print(results)
 
